9dc0513e6eee1410d79038fccd5301dc08b5a23b
Urimoto-Takaaki <69062224+Urimoto-Takaaki@users.noreply.github.com>
2020-08-06 19:01:23 +0900

    resource追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
   #一覧表示
-  get '/users', to: 'users#index'
-  get '/users/new', to: 'users#new'
-  post '/users', to: 'users#create'
-  get '/users/:id/edit', to: 'users#edit'
-
-  # 以下のルーティングを追加
-  patch '/users/:id', to: 'users#update'
-
-  delete '/users/:id', to: 'users#destroy'
-
+resource : users
 end
